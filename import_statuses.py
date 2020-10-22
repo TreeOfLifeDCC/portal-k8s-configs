c7947948e64146ed43c7bdbec5f4f9db37b27f56
Alexey Sokolov <alexey@ebi.ac.uk>
2020-10-22 20:25:23 +0100

    Adding import_statuses script

---
+++ import_statuses.py
@@ -0,0 +1,59 @@
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(['elasticsearch:9200'])
+
+
+def main():
+    data = es.search(index='dtol', size=10000)
+    existing_data = get_existing_data()
+    for record in data['hits']['hits']:
+        tmp = dict()
+        tmp['organism'] = record['_source']['organism']
+        tmp['commonName'] = record['_source']['commonName']
+        tmp['biosamples'] = 'Done'
+        tmp['raw_data'] = check_raw_data_status(record['_source']['experiment'])
+        tmp['mapped_reads'] = check_mapped_reads_status(record['_source'][
+                                                            'experiment'])
+        tmp['assemblies'] = check_assemblies(record['_source']['assemblies'])
+        # TODO: add an ENSEMBL check
+        tmp['annotation'] = 'Waiting'
+        if tmp['organism'] in existing_data:
+            es.index('statuses', tmp, id=existing_data[tmp['organism']])
+        else:
+            es.index('statuses', tmp)
+
+
+def get_existing_data():
+    existing_data = dict()
+    data = es.search(index='statuses', size=10000)
+    for record in data['hits']['hits']:
+        existing_data[record['_source']['organism']] = record['_id']
+    return existing_data
+
+
+# TODO: refactor
+def check_raw_data_status(record):
+    if len(record[0]['fastq_ftp']) != 0:
+        return 'Done'
+    else:
+        return 'Waiting'
+
+
+# TODO: refactor
+def check_mapped_reads_status(record):
+    if len(record[0]['submitted_ftp']) != 0:
+        return 'Done'
+    else:
+        return 'Waiting'
+
+
+# TODO: refactor
+def check_assemblies(record):
+    if len(record) != 0:
+        return 'Done'
+    else:
+        return 'Waiting'
+
+
+if __name__ == "__main__":
+    main()
